0e06b34f6f595259c2edf4cea93bfec11d88162f
QuillVest <QuillVest.QV@gmail.com>
2025-01-05 19:45:29 -0500

    Add files via upload

---
+++ scripts/prefabs/wurrow.lua
@@ -0,0 +1,95 @@
+local MakePlayerCharacter = require "prefabs/player_common"
+
+local assets = {
+    Asset("SCRIPT", "scripts/prefabs/player_common.lua"),
+}
+
+SetSharedLootTable("wurrow",
+{
+    { "wormlight",  1.00 },
+})
+
+TUNING.WURROW_HEALTH = 175
+TUNING.WURROW_HUNGER = 225
+TUNING.WURROW_SANITY = 125
+
+TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT.WURROW = {
+	"wormlight_lesser",
+	"wormlight_lesser",
+	"wormlight_lesser",
+	"slurper_pelt",
+	"slurper_pelt",
+}
+
+local start_inv = {}
+for k, v in pairs(TUNING.GAMEMODE_STARTING_ITEMS) do
+    start_inv[string.lower(k)] = v.WURROW
+end
+local prefabs = FlattenTree(start_inv, true)
+
+local function onbecamehuman(inst)
+	inst.components.locomotor:SetExternalSpeedMultiplier(inst, "wurrow_speed_mod", 1)
+end
+
+local function onbecameghost(inst)
+   inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "wurrow_speed_mod")
+end
+
+local function onload(inst)
+    inst:ListenForEvent("ms_respawnedfromghost", onbecamehuman)
+    inst:ListenForEvent("ms_becameghost", onbecameghost)
+
+    if inst:HasTag("playerghost") then
+        onbecameghost(inst)
+    else
+        onbecamehuman(inst)
+    end
+end
+
+local common_postinit = function(inst) 
+	inst:AddTag("monster")
+	inst:AddTag("worm")
+	inst:AddTag("cavedweller")
+	inst.MiniMapEntity:SetIcon( "wurrow.tex" )
+end
+
+local master_postinit = function(inst)
+    inst.starting_inventory = start_inv[TheNet:GetServerGameMode()] or start_inv.default
+
+	inst.components.combat.shouldavoidaggrofn = function(attacker, inst) return attacker.prefab ~= 'worm' end
+	inst.components.combat.shouldavoidaggrofn = function(attacker, inst) return attacker.prefab ~= 'worm_boss' end
+
+	inst.soundsname = "wormwood"
+	
+	inst.components.health:SetMaxHealth(TUNING.WURROW_HEALTH)
+	inst.components.hunger:SetMax(TUNING.WURROW_HUNGER)
+	inst.components.sanity:SetMax(TUNING.WURROW_SANITY)
+	
+    inst.components.combat.damagemultiplier = 1
+	
+	inst.components.hunger.hungerrate = 1.5 * TUNING.WILSON_HUNGER_RATE
+
+	inst.components.sanity.no_moisture_penalty = true
+
+	inst.components.foodaffinity:AddPrefabAffinity("unagi", TUNING.AFFINITY_15_CALORIES_SMALL)
+
+	local foodaffinity = inst.components.foodaffinity
+    foodaffinity:AddFoodtypeAffinity(FOODTYPE.MEAT,        1.33)
+    foodaffinity:AddPrefabAffinity  ("wormlight",          1.33)
+	foodaffinity:AddPrefabAffinity  ("wormlight_lesser",   1.33)
+
+	inst.components.eater:SetDiet({ FOODTYPE.VEGGIE, FOODTYPE.BERRY, FOODTYPE.SEEDS, FOODTYPE.MEAT })
+	if inst.components.eater ~= nil then
+        inst.components.eater:SetStrongStomach(true)
+        inst.components.eater:SetCanEatRawMeat(true)
+    end
+	
+	inst.OnLoad = onload
+    inst.OnNewSpawn = onload
+
+	inst.components.sanity.night_drain_mult = 0
+	inst.components.sanity:AddSanityAuraImmunity("worm")
+	inst.components.sanity:AddSanityAuraImmunity("worm_boss")
+end
+
+return MakePlayerCharacter("wurrow", prefabs, assets, common_postinit, master_postinit, prefabs)

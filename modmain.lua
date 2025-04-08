e1a6ea670967b2cd37f954a539733a55a9732623
QuillVest <QuillVest.QV@gmail.com>
2025-04-08 13:21:37 -0400

    Update
    Part 17

--- modmain.lua
+++ modmain.lua
@@ -63,12 +63,23 @@ local skin_modes = {
 }
 ------------------------------------------------------------------------------------------------------------
 
---- Courtesy of zhuyifei1999 & ClumsyPenny ↓
+--- Courtesy of zhuyifei1999, ClumsyPenny & Luкaട ↓
 AddAction("BURROW", "Burrow", function(act)
     if act.doer ~= nil and act.doer:HasTag("wurrow") then
         return true
     end
 end)
+
+AddStategraphActionHandlers {
+    ActionHandler(ACTIONS.BURROW,
+        function(inst, action)
+            return action.invobject == nil and inst:HasTag("wurrow") and "burrow_enter"
+        end),
+    ActionHandler(ACTIONS.TUNNEL,
+        function(inst, action)
+            return action.invobject == nil and inst:HasTag("wurrow") and "burrowing"
+        end),
+}
 
 AddComponentAction("BURROW_RCLICK", "Burrow", function(doer, actions, _right)
     if doer.prefab == "wurrow" then
@@ -76,8 +87,119 @@ AddComponentAction("BURROW_RCLICK", "Burrow", function(doer, actions, _right)
     end
 end)
 
-AddStategraphActionHandler("wurrow", GLOBAL.ActionHandler(GLOBAL.ACTIONS.BURROW, "dolongaction"))
-AddStategraphActionHandler("wurrow_client", GLOBAL.ActionHandler(GLOBAL.ACTIONS.BURROW, "dolongaction"))
+AddStategraphState {
+    State{
+        name = "burrow_pre",
+        tags = {"busy"},
+
+        onenter = function(inst)
+            inst.components.locomotor:Stop()
+            inst.AnimState:PlayAnimation("jumpin")
+            inst.AnimState:PlayAnimation("despawn")
+            local buffaction = inst:GetBufferedAction()
+            if buffaction ~= nil and buffaction.pos ~= nil then
+                inst:ForceFacePoint(buffaction:GetActionPoint():Get())
+            end
+        end,
+
+        timeline = {
+            FrameEvent(10, function(inst)
+                SpawnAt("shovel_dirt", inst)
+            end),
+            FrameEvent(30, function(inst)
+                SpawnAt("shovel_dirt", inst)
+            end),
+            FrameEvent(40, function(inst)
+                inst.sg:RemoveStateTag("busy")
+            end)
+        },
+
+        events = {
+            EventHandler("animover", 
+                function(inst)
+                    if inst.AnimState:AnimDone() and not inst:PerformBufferedAction() then
+                        inst.sg:GoToState("burrowing")
+                    end
+                end)
+        },
+
+        onexit = function(inst)
+            if inst.sg:HasStateTag("busy") then
+                inst.SoundEmitter:KillSound("rumble_lp")
+            end
+        end,
+    },
+}
+
+AddStategraphState {
+    State{
+        name = "burrowing",
+        tags = { "moving", "canrotate", "dirt", "invisible" },
+    
+        onenter = function(inst)
+            inst.components.health:SetInvincible(true)
+            inst.DynamicShadow:Enable(false)
+            inst.components.locomotor.walkspeed = 4
+            inst.components.locomotor:SetSlowMultiplier( 1 )
+            inst.components.locomotor:SetTriggersCreep(false)
+            inst.components.locomotor.pathcaps = { ignorecreep = true, ignorebridges = true, }
+            inst.components.locomotor:WalkForward()
+            inst.AnimState:PlayAnimation("walk_loop")
+            if not inst.SoundEmitter:PlayingSound("walkloop") then
+                inst.SoundEmitter:PlaySound("dontstarve/creatures/worm/move", "walkloop")
+            end
+         end,
+    
+        timeline = {
+            TimeEvent(0, function(inst) inst.SoundEmitter:PlaySound("dontstarve/creatures/worm/dirt") end),
+            TimeEvent(10 * FRAMES, function(inst) inst.SoundEmitter:PlaySound("dontstarve/creatures/worm/dirt") end),
+            TimeEvent(20 * FRAMES, function(inst) inst.SoundEmitter:PlaySound("dontstarve/creatures/worm/dirt") end),
+         },
+
+        events = {
+            EventHandler("animover", function(inst)
+                inst.sg.statemem.walking = true
+                inst.sg:GoToState("walk")
+            end),
+        },
+    
+        onexit = function(inst)
+            if not inst.sg.statemem.walking then
+                inst.SoundEmitter:KillSound("walkloop")
+            end
+        end,
+    },
+}
+
+AddStategraphState {
+    State{
+        name = "burrow_out",
+        tags = {"busy"},
+        onenter = function(inst)
+            inst.components.locomotor:Stop()
+            inst.AnimState:PlayAnimation("jumpout")
+            local buffaction = inst:GetBufferedAction()
+                if buffaction ~= nil and buffaction.pos ~= nil then
+                    inst:ForceFacePoint(buffaction:GetActionPoint():Get())
+                end
+        end,
+        
+        onexit = function(inst)
+        end,
+
+        events =
+        {
+            EventHandler("animover", function(inst)
+                if inst.AnimState:AnimDone() and not inst:PerformBufferedAction() then
+                    inst.sg:GoToState("idle")
+                end
+            end),
+        },
+    },
+}
+
+AddStategraphActionHandler("wilson", GLOBAL.ActionHandler(GLOBAL.ACTIONS.BURROW, "dolongaction"))
+AddStategraphActionHandler("wilson_client", GLOBAL.ActionHandler(GLOBAL.ACTIONS.BURROW, "dolongaction"))
 
 ------------------------------------------------------------------------------------------------------------
 
